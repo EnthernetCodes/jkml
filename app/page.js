bba19fbd6360666d662232d6072ed699d152bb34
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2025-03-10 10:33:36 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -90,7 +90,7 @@ useEffect(()=> {
   return (
 
     <div className='w-screen h-screen'>
-      <Image src={"/images/newbg.webp"} width={2000} height={2000} alt='loading..' className='w-full h-full object-cover object-top'/>
+      <Image src={"/images/newbg.webp"} width={2000} height={2000} alt='loading..' className='w-full h-full object-cover object-top-left'/>
    
             <div className={`warn-cont p-5`}>
               <div className={`warn-card p-5 max-sm:p-2 sm:mr-[200px]`} style={{transform: animateIn? "translateY(0)" : " translateY(-50px)", opacity: animateIn ? "1" : "0"}}>
