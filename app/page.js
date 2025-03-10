a583e0a8291d4a1467fa62222db67ae7f1fb4f2d
EnthernetCodes <enthernetcodebiz@gmail.com>
2025-03-10 08:13:35 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -89,8 +89,8 @@ useEffect(()=> {
 
   return (
 
-    <div className='w-screen h-screen object-cover'>
-      <Image src={"/images/newbg.webp"} width={2000} height={2000} alt='loading..' className='w-full h-full'/>
+    <div className='w-screen h-screen'>
+      <Image className="object-cover" src={"/images/newbg.webp"} width={2000} height={2000} alt='loading..' className='w-full h-full'/>
    
             <div className={`warn-cont p-5`}>
               <div className={`warn-card p-5 max-sm:p-2`} style={{transform: animateIn? "translateY(0)" : " translateY(-50px)", opacity: animateIn ? "1" : "0"}}>
